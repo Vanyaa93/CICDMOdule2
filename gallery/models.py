de02d1ed0a0df1dd5369acd243f8fc8de71a2deb
Vanyaa93 <aasihmin021@knu.ua>
2025-05-23 09:18:34 +0300

    Add Image model

--- gallery/models.py
+++ gallery/models.py
@@ -5,3 +5,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField(Category)
+    created_date = models.DateField(auto_now_add=True)
+    age_limit = models.PositiveIntegerField()
